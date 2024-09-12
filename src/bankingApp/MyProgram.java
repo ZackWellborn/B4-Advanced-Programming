722fe503780a406619bd46d6d2c1d34aa6a7668e
ZackWellborn <111790777+ZackWellborn@users.noreply.github.com>
2024-09-12 12:54:55 -0600

    Create MyProgram.java

---
+++ src/bankingApp/MyProgram.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.*;  
+//use processing ig
+public class MyProgram{
+    public static void main(String[] args)
+    {
+        ArrayList<String> users = new ArrayList<String> ();
+        users.add("Jimmy");
+        // Account Jimmy = new Account();        cryign rn ;;
+        // Account Alice = new Account(); 
+        // Account Max = new Account();
+        // Account Bob = new Account();
+        // Account Charlie = new Account();
+        for (String c : users) {
+             // Account c = new Account();
+             System.out.println(c);
+        }
+        System.out.println("Hello, welcome to our banking application!");
+        Account filler = new Account(5.0, "bill");
+        //System.out.println("Enter your account name below to create a new account.");
+        filler.setName();
+        System.out.println("Accounts: Jimmy, Alice, Max, Bob, Charlie");
+    }
+    //scanner.close();
+}
+
+
+// https://www.w3schools.com/java/java_arrays.asp
+// https://www.w3schools.com/java/java_arraylist.asp
+// https://www.w3schools.com/java/ref_keyword_float.asp
+// https://www.geeksforgeeks.org/java-main-method-public-static-void-main-string-args/
+// https://www.w3schools.com/java/java_methods.asp
+// https://www.w3schools.com/java/java_classes.asp
+// https://stackoverflow.com/questions/27729607/creating-an-array-of-classes-in-java
+// https://www.w3schools.com/java/java_for_loop.asp             // Account c = new Account();
