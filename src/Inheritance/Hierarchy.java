dc93dcacd62f6b3479d0c6c0f90d4a1e4a7b6fa6
ZackWellborn <111790777+ZackWellborn@users.noreply.github.com>
2024-10-15 23:41:08 -0600

    Update Hierarchy.java
    reptilesss

--- src/Inheritance/Hierarchy.java
+++ src/Inheritance/Hierarchy.java
@@ -3,7 +3,8 @@ public class Hierarchy {
         // Create an instance of Fish
         Fish swordFish = new Fish("blue", "blub", "Swordfish", true, false, "Xiphias gladius", 80);
         Bird raven = new Bird("black", "caw", "raven", false, true, 4, 25);
-    
+        Reptile Snake = new Reptile("variable", "hisss", "Snek", true, false, false, 0);
+
         System.out.println("A swordfish's call is " + swordFish.getCall());    // Inherited from Animal
         System.out.println("The swordfish's color is " + swordFish.getColor());   // Inherited from Animal
         System.out.println("The genus of a swordfish is " + swordFish.getGenus() + "\n");     // Inherited from Fish
@@ -12,11 +13,12 @@ public class Hierarchy {
         
         System.out.println("Here's some basic info on a swordfish: \n" + swordFish.printInfo() + "\n"); 
         System.out.println("Now here's some basic info on a raven: \n" + raven.printInfo() + "\n");
+        System.out.println("Now here's some basic info on a snake: \n" + Snake.printInfo() + "\n");
 
         System.out.println("Does a swordfish have wings? \n" + swordFish.getHasWings());
         System.out.println("What if swordfish evolved to grow wings?");
         swordFish.setHasWings(true);
-        System.out.println("Now do swordfish have wings? \n" + swordFish.getHasWings());
+        System.out.println("Now do swordfish have wings? \n" + swordFish.getHasWings() + " :D");
     }
 
 //lol;lmao,
